6a968a33de7f6f3944c088eae7e602b7c3dfb96d
Tim Snyder <snyder.tim@gmail.com>
2022-06-07 22:45:27 +0000

    az-attestation add account and container creation
    still uses a hardcoded container name but don't have to figure out the portal
    to create the account and container

--- cl_firesim/scripts/azure-attestation.sh
+++ cl_firesim/scripts/azure-attestation.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-set -x
+set -xe
 
 input_file=$1
 output_file=$2
@@ -10,7 +10,7 @@ if [[ $# != 2 ]]; then
 fi
 
 acct_name=firesim
-container=firesim-biancolin
+container=firesim-snyder
 
 # I've justed hashed the input here so that xclbins don't conflict in a flat namespace.
 # I'm not actually sure if identical inputs to Vitis will produce a
@@ -18,19 +18,35 @@ container=firesim-biancolin
 dcp_sha=$(sha1sum $input_file | awk '{ print $1 }')
 container_input_file="${dcp_sha}.xclbin"
 
-# Note, there will be a "bit.xclbin" generated that can be used with a local U250.
-container_output_file="${dcp_sha}.azure.xclbin"
+# can check existance of the account and create if not there
+if ! az storage account show --name $acct_name >/dev/null; then
+    az storage account create --name $acct_name
+fi
 
 # I picked this delta arbitrarily. For Rocket-based designs the process
 # finishes in < 30m
 end=$(date -u -d "6 hours" '+%Y-%m-%dT%H:%MZ')
+
+acct_sas=$(az storage account generate-sas \
+         --account-name $acct_name \
+	 --expiry $end \
+	 --resource-types c \
+	 --services bfqt \
+	 --https-only --permissions rwc --output tsv)
+
+if [[ "$(az storage container exists --account-name $acct_name --sas-token \"$acct_sas\" --name $container | jq -r '.exists')" != "true" ]]; then
+    az storage container create --account-name $acct_name --sas-token "$acct_sas" --name $container
+fi
+
+# Note, there will be a "bit.xclbin" generated that can be used with a local U250.
+container_output_file="${dcp_sha}.azure.xclbin"
+
 SAS=$(az storage container generate-sas \
     --account-name $acct_name \
     --name $container \
     --expiry $end \
     --https-only --permissions rwc --output tsv)
 
-# This presupposes the existence of the container.
 az storage blob upload \
     --account-name $acct_name \
     --container-name $container \
@@ -40,6 +56,7 @@ az storage blob upload \
 mkdir -p attestation
 cd attestation/
 wget https://fpgaattestation.blob.core.windows.net/validationscripts/validate.zip
+sha256sum validate.zip
 unzip -o validate.zip
 
 bash scripts/validate-fpgaimage.sh \
